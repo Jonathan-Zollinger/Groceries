aa288c13873425a6d85fe76f0b8dc9e3ef6ccb7d
Jonathan Zollinger <jonathan.zollinger@microfocus.com>
2020-07-31 09:02:25 -0600

    successfully pulls unparsed html from site with jsoup

--- src/crawls/WalmartGroceryCrawl.java
+++ src/crawls/WalmartGroceryCrawl.java
@@ -1,7 +1,10 @@
 package crawls;
+import org.jsoup.Jsoup;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.io.PrintWriter;
 //import org.selenium.By;
 
 public class WalmartGroceryCrawl
@@ -12,11 +15,43 @@ public class WalmartGroceryCrawl
         String targetSite = "https:/www.walmart.com/grocery/products?aisle=1255027787131_4364938242&&page=1";
 
         //define the webdriver file location
-        System.setProperty("webdriver.gecko.driver", "\\Scripts\\webdrivers\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver", "\\Scripts\\webdrivers\\geckodriver-v0.27.0.exe");
 
-
+        //create an instance of FirefoxDriver
         WebDriver driver = new FirefoxDriver();
 
+        //open the website that is specified as the targetSite
         driver.get(targetSite);
+
+        //define html text filename
+        String htmlFile = targetSite.substring(targetSite.indexOf("www")+4,targetSite.indexOf(".com")) + "_html.txt";
+
+        //pull html, store to string
+        String html = null;
+        try
+        {
+            html = Jsoup.connect(driver.getCurrentUrl()).get().html();
+        }catch(Exception e)
+        {
+            System.out.printf("can't pull html from %s",targetSite);
+        }
+        //save html source code to text file
+        PrintWriter writer = null;
+        try
+        {
+            writer = new PrintWriter(htmlFile);
+            writer.print(html);
+            writer.flush();
+            writer.close();
+            System.out.println(htmlFile + " successfully written");
+
+        }catch (Exception e)
+        {
+            System.out.print(htmlFile + " not written");
+        }
+
+        //close the driver
+        driver.quit();
+
     }
 }
