3b582c188e7b4b7e9242ef6f9256d9ee778f5281
Jonathan Zollinger <jonathan.zollinger@microfocus.com>
2020-08-04 16:12:12 -0600

    merges all .java files. beautifyHTML not working

--- src/crawls/WalmartGroceryCrawl.java
+++ src/crawls/WalmartGroceryCrawl.java
@@ -1,14 +1,13 @@
 package crawls;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import java.io.PrintWriter;
-import org.openqa.selenium.safari.SafariDriver;
 
+import java.io.File;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class WalmartGroceryCrawl
 {
@@ -30,6 +29,10 @@ public class WalmartGroceryCrawl
         String testHTML = driver.getPageSource();
         String testHTMLFileName = "testHTML.txt";
 
+        //close the driver
+        driver.quit();
+
+        testHTML = beautifyHTML(testHTML);
 
         //save html to a file
         PrintWriter writer = null;
@@ -44,8 +47,66 @@ public class WalmartGroceryCrawl
         }catch (Exception e) {
         }
 
-        //close the driver
-        driver.quit();
 
-    }
+    }//end public static void main(String[] args)
+
+    public static String openFile(String fileLocation) {
+
+        File inFile = new File(fileLocation);
+        Scanner file = null;
+        String sourceText = null;
+        try {
+            file = new Scanner(inFile);
+
+            while (file.hasNext()) {
+                sourceText = sourceText + file.nextLine();
+            }//end of while(file.hasNext())
+
+        } catch (Exception e) {
+
+            System.out.printf("File Error: File '%s' not found! " +
+                    "Please save the list before trying to open it.", fileLocation);
+
+            System.out.printf("%s successfully pulled into program", fileLocation);
+
+        } //end of catch (Exception e)
+        return sourceText;
+    }//end openFile(String fileLocation)
+
+    //this method beautifies html code
+    public static String beautifyHTML(String input) {
+
+        //define variables
+        String testHTML = null;
+        String beautHTML = null;
+        WebDriver driver = new FirefoxDriver();
+        WebElement beautifier = driver.findElement(By.id("source"));
+        WebElement formatButton = driver.findElement(By.id("format"));
+        String targetSite = "https://www.bing.com/search?q=html%20beautifier";
+
+        //define the webdriver file location
+        System.setProperty("webdriver.gecko.driver", "\\Scripts\\webdrivers\\geckodriver-v0.27.0.exe");
+
+        //determine if the string is an html doc or filename
+        if (input.contains(".txt") || input.contains(".html")) {
+            testHTML = openFile(input);
+        } else {
+            testHTML = input;
+        }//end if-else(if (input.contains(".txt") || input.contains(".html")))
+
+        //open bing
+        driver.get("www.google.com");
+
+        //paste raw html
+        beautifier.click();
+        beautifier.clear();
+        beautifier.sendKeys(testHTML);
+        formatButton.click();
+
+        //retrieve beautified html
+        beautHTML = beautifier.getText();
+
+        System.out.println("successfully beautified HTML");
+        return beautHTML;
+    }//end public static String beautify(String input)
 }
