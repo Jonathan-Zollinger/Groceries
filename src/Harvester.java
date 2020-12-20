de62600dd251eb8caef3627d7c36dfd43a1c4114
Jonathan Zollinger <jonathan.zollinger@outlook.com>
2020-12-19 20:10:10 -0700

    WIP: cleans up harvester and store classes

--- src/Harvester.java
+++ src/Harvester.java
@@ -5,27 +5,18 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+public class Harvester {
+    WebDriver driver;
+        public static void main(String[] args) {
 
-    public class Harvester {
-        public static void Click(WebDriver driver, By by){
-            WebDriverWait wait = new WebDriverWait(driver, 2);
-            wait.until(ExpectedConditions.elementToBeClickable(by));
-            driver.findElement(by).click();
         }
 
-        public static WebDriver getHeadlessDriverSpecifyDownloadDirectory(
-                String driverPath, String downloadFilepath,String mimeType) {
+        public static WebDriver getDriver(
+                String driverPath,
+                boolean isHeadless,
+                String downloadFilepath,
+                String mimeType) {
 
             if (mimeType == null){
                 mimeType = "application/x-gzip";
@@ -47,7 +38,7 @@ import java.util.concurrent.TimeUnit;
 
             //set Firefox options
             FirefoxOptions options = new FirefoxOptions();
-            options.setHeadless(false); //run headless version of firefox
+            options.setHeadless(false); //true == headless version of firefox
 
             //include profile in options
             options.setProfile(profile); //include all the profile settings in this option set
@@ -57,57 +48,44 @@ import java.util.concurrent.TimeUnit;
 
         }//end public static WebDriver getHeadlessDriverSpecifyDownloadDirectory(String downloadFilepath, String mimeType)
 
-        public static void closeWalmartOnboardingPrompt(WebDriver driver){
-            //define the selection
-            final By onboardBtn = By.className("OnboardingModal__closeBtn___2xhJM");
 
-            waitAndClick(driver, onboardBtn, "onboarding button");
+        public static WebDriver getDownloadingDriver(
+                String driverPath,
+                boolean isHeadless,
+                String downloadFilepath,
+                String mimeType) {
 
-        }//end public static void closeWalmartOnboardingPrompt(WebDriver driver)
+            if (mimeType == null){
+                mimeType = "application/x-gzip";
+            }
+            System.setProperty("webdriver.gecko.driver", driverPath);
+            FirefoxProfile profile = new FirefoxProfile();
 
-        public static void waitAndClick(WebDriver driver, By clicker, String actionName){
-            try {
-                //wait for the element to be clickable
-                new WebDriverWait(driver, 4)
-                        .until(ExpectedConditions
-                                .elementToBeClickable(clicker));
-                //now click the element
-                driver.findElement(clicker).click();
-                System.out.printf("driver clicked the %s%n",actionName);
-            }catch (Exception e){
-                System.out.printf("driver failed to click the %s%n",actionName);
-            }//end try/catch for WebDriverWait->clicker to be clickable
-        }//end public static void waitAndClick(WebDriver driver, By clicker, String actionName)
+            //set Firefox profile preferences
+            /* it seems like you could get away without changing the browser.download.folderList preference to "2", but
+            without it we can't change the browser.download.dir preference. */
+            profile.setPreference("browser.download.folderList", 2);//uses last download directory as destination directory
+            profile.setPreference("browser.download.dir",downloadFilepath); //if path = invalid, goes to downloads folder
 
-        public static void clickThroughLinkText(WebDriver driver, ArrayList<String> selectors){
-            for(int i = 0; i < selectors.size(); i++){
-            /*because the webpage may respond slower than our program, loop through a
-                couple times if the driver action fails */
-                int count = 2;
-                while(count > 0) {
-                    try {
-                        driver.findElement(
-                                By.linkText(selectors.get(i)))
-                                .click();
-                        System.out.printf("driver clicked \"%s\"%n",selectors.get(i));
-                        break;
-                    } catch (Exception e) {
-                        System.out.printf("driver failed to click selector \"%s\", " +
-                                "will try %dx more%n",selectors.get(i),count-1);
-                        count --;
-                        //wait 1/2 second till looping through
-                        try{
-                            TimeUnit.MILLISECONDS.sleep(500);}catch (Exception d){System.out.print("sleep failed");}
-                    }//end catch (Exception e)
-                }//end int count = 5; while(count > 0)
-            }//end for(int i = 0; i < selectors.size(); i++)
-        }//end public static void clickThroughCssArrayList(WebDriver driver, ArrayList<String> list)
+        /* the second argument in these preferences is what's called a "mimetype" of a filetype.
+        I find the easiest way to figure the proper mimetype for your use is to follow Florent B's suggestion
+        and look at the network panel under the developer tools under a manual run through. https://bit.ly/2FEYB0p */
+            profile.setPreference("browser.helperApps.neverAsk.openFile",mimeType);
+            profile.setPreference("browser.helperApps.neverAsk.saveToDisk",mimeType);
 
-        public static void clickThroughByList(WebDriver driver, ArrayList<Array> selectors){
-            for(int i = 0; i < selectors.size(); i++){
+            //set Firefox options
+            FirefoxOptions options = new FirefoxOptions();
+            options.setHeadless(isHeadless); //run headless version of firefox
 
-            }//end for(int i = 0; i < selectors.size(); i++)
-        }//end public static void clickThroughCssArrayList(WebDriver driver, ArrayList<String> list)
-    }
+            //include profile in options
+            options.setProfile(profile); //include all the profile settings in this option set
 
-}
+            //initiate new instance of firefox
+            return new FirefoxDriver(options); //call the driver w/ our specified options (and profile).
+
+        }//end public static WebDriver getHeadlessDriverSpecifyDownloadDirectory(String downloadFilepath, String mimeType)
+    }//end Harvester Class
+
+
+
+
--- src/Store/Store.java
+++ src/Store/Store.java
@@ -1,37 +1,59 @@
 package Store;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.lang.*;
-import java.util.Properties;
 
-public class Store {
+public abstract class Store {
 
-    //fields
-    private static String storageDump;
-    private static String driverFile;
-    static {
-        if (System.getProperty("os.name").startsWith("Windows")){
-            storageDump = "C:\\Scripts\\Price_Tracker";
-            driverFile = "geckodriver-v0.27.0-win64.exe";
-        }else{//is linux
-            storageDump = "/Price_Tracker";
-            driverFile = "geckodriver-v0.27.0-linux64";
-        }// end if(System.getProperty("os.name").startsWith("Windows")
-    }
-    //abstract fields
-    private String homeURL;
+    //values
+    String waitingLog = "Waiting for %s to be clickable on the %s page"; //element, toString
+    String clickedLog = "Clicked %s on the %s page"; //element, toString
+
+    //abstract values
+    String homeURL = "google.com";
+
+    //selectors
+
+    //selector methods
 
     //constructors
-    public Store(){
-        homeURL = "zollyLobby.com";
+    public Store(WebDriver driver) {
+        driver.get(homeURL);
     }
 
     //methods
-
-    public void setHomeURL(String homeURL) {
-        this.homeURL = homeURL;
+    public void Click(WebDriver driver, By by, String item){
+        print(String.format(waitingLog,item,toString()),true);
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+        driver.findElement(by).click();
+        print(String.format(clickedLog,item,toString()),true);
     }
+    public void waitAndClick(WebDriver driver, By by, String action) {
+    //wait for the element to be clickable
+    new WebDriverWait(driver, 4)
+            .until(ExpectedConditions
+                    .elementToBeClickable(by));
+    //now click the element
+    driver.findElement(by).click();
+    System.out.printf("driver clicked the %s%n", action);
 
-    public String getHomeURL() {
-        return homeURL;
+    }//end public static void waitAndClick(WebDriver driver, By by, String action)
+    private void print(String string, boolean isPrintLine){
+        if (isPrintLine){
+            System.out.println(string);
+        }else {
+            System.out.print(string);
+        }
+    }//end print
+
+    //abstract methods
+    String toString(){
+        return "Store";
     }
-}
+}//end store
+
